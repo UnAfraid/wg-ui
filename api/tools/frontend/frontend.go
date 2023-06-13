29f003222ec2f0dc156161ab8756c71d0a82d0da
UnAfraid <unafraid89@gmail.com>
2023-06-13 15:36:11 +0300

    Handle frontend related 404s by serving /

--- api/tools/frontend/frontend.go
+++ api/tools/frontend/frontend.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"io/fs"
 	"net/http"
+	"os"
 )
 
 //go:embed app/*
@@ -14,5 +15,20 @@ func Handler() http.Handler {
 	if err != nil {
 		panic(err)
 	}
-	return http.StripPrefix("/", http.FileServer(http.FS(appFs)))
+	return http.StripPrefix("/", handle404(http.FS(appFs)))
+}
+
+func handle404(root http.FileSystem) http.Handler {
+	fileServer := http.FileServer(root)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		f, err := root.Open(r.URL.Path)
+		if err != nil {
+			if os.IsNotExist(err) {
+				r.URL.Path = "/"
+			}
+		} else {
+			_ = f.Close()
+		}
+		fileServer.ServeHTTP(w, r)
+	})
 }
